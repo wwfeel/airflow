e64c5c052e404fbcacbba87e478ade81b02881d6
airmario <wwfeel@gmail.com>
2024-06-30 19:39:26 +0900

    dags_bash_python_with_xcom upload

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -22,7 +22,7 @@ with DAG(
         env={'PUSHED_VALUE':"{{ ti.xcom_pull(key='bash_pushed') }}",
             'RETURN_VALUE':"{{ ti.xcom_pull(task_ids='bash_push') }}"},
         bash_command="echo $PUSHED_VALUE && echo $RETURN_VALUE ",
-        do_xcom_push=False
+        do_xcom_push=False # xcom에 저장 금지하는 구문
     )
 
     bash_push >> bash_pull
