56706eb5df0a2d97e6f2b3ded6c096834533ab9b
airmario <wwfeel@gmail.com>
2024-06-30 12:11:45 +0900

    dag_bash_with_macro_eg1, eg2 upload

---
+++ dags/dag_bash_with_macro_eg1.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dag_bash_with_macro_eg1",
+    schedule="10 0 L * *",
+    start_date=pendulum.datetime(2024, 5, 31, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    # START_DATE: 전월 말일, END_DATE: 1일전
+    bash_task_1 = BashOperator(
+        task_id='bash_task_1',
+        env={'START_DATE':'{{ data_interval_start.in_timezone("Asia/Seoul") | ds }}',
+             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macro.datautil.relativedelta.relativedelta(days=1)) | ds }}'
+        },
+        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
+    )
