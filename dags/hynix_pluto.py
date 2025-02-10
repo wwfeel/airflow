599d916574bf288a6c8b41ee391cf1d1440dcd92
yfsong <yfsong@naver.com>
2025-02-10 22:29:03 +0900

    sftp_hook sh

--- dags/hynix_pluto.py
+++ dags/hynix_pluto.py
@@ -3,7 +3,7 @@ import pendulum
 from airflow.operators.python import PythonOperator
 from airflow.providers.sftp.operators.sftp import SFTPHook, SFTPOperator
 
-ssh_hook = SFTPHook(ssh_conn_id=None, username="SYP", remote_host="154.53.56.101", cmd_timeout=None)
+sftp_hook = SFTPHook(ssh_conn_id=None, username="SYP", remote_host="154.53.56.101", cmd_timeout=None)
 
 with DAG(
         dag_id='hynix_pluto',
@@ -14,7 +14,7 @@ with DAG(
     
     put_file = SFTPOperator(
         task_id="put_file",
-        ssh_hook = ssh_hook,
+        ssh_hook = sftp_hook,
         local_filepath="/opt/airflow/config/10080.py",
         remote_filepath="10080.py",
         operation="get",
